4e6abdd7647a76159b3aedca3b279ec83a19bdc2
MolassesLover <mail@molasses.love>
2026-04-15 17:56:04 +0200

    Added a config for the update extension

--- hosts/daniels-pc/home.nix
+++ hosts/daniels-pc/home.nix
@@ -4,6 +4,10 @@
 {
   home.stateVersion = "23.11";
 
+  home.file.".config/molasses/update-nix/config.json" = {
+    source = ./../../config/molasses/update-nix/config.json;
+  };
+
   home.packages = [
     pkgs._1password-gui
     pkgs.signal-desktop
